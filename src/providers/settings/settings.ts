a743fdb222904a95159c4412ade80c21bd3bd444
amit <amitmoryossef@gmail.com>
2017-06-25 21:33:05 +0300

    feat(settings): add settings

---
+++ src/providers/settings/settings.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import {Platform} from "ionic-angular";
+
+interface Settings {
+  isSpeak: boolean;
+}
+
+@Injectable()
+export class SettingsProvider {
+  settings: Settings = {
+    isSpeak: true
+  };
+
+  constructor(platform: Platform, private storage: Storage) {
+    platform.ready().then(this.load.bind(this));
+  }
+
+  private load(): Promise<any> {
+    return this.storage.get("settings")
+        .then(settings => JSON.parse(settings))
+        .then(settings => Object.keys(settings).forEach(key => this.settings[key] = settings[key]))
+        .catch(() => {});
+  }
+
+  private save(): Promise<any> {
+    return this.storage.set("settings", JSON.stringify(this.settings));
+  }
+
+  set(setting: string, val: any) {
+    this.settings[setting] = val;
+    this.save();
+  }
+
+  get(setting) {
+    return this.settings[setting];
+  }
+}
